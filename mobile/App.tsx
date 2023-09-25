b4f36c660e0daeec4144770b1a0e92e4335ec9ca
Atilio <atilio.jesus@grupoboticario.com.br>
2023-09-25 09:11:16 -0300

    Add mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,16 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+});
